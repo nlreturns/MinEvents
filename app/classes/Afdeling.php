ae3cdc830a0847322d28a46bbec4c44771790b1e
Donneh <donnyvw@hotmail.nl>
2014-11-11 16:38:43 +0100

    Wat verbeteringen
    Meer documentatie toegevoegd en deel van bestaande aangepast.
    Paar !empty toegevoegd

--- app/classes/Afdeling.php
+++ app/classes/Afdeling.php
@@ -1,67 +1,83 @@
 <?php
+
 namespace minevents\app\classes;
+
 /**
- * DbDomein connects the application to the database using Database.php
+ * Deze klasse word gebruikt voor het toevoegen, aanpassen
+ * en verwijderen van Afdelingen.
+ * Een afdeling is een deel van M in Events.
+ * Bijvoorbeeld: Bowlingbaan, Bar en Restaurant.
+ *
+ * @package    minevents\app\classes
+ * @author     Donny van Walsem <donnehvw@gmail.com>
+ * @version    1.0
+ * @see        NetOther, Net_Sample::Net_Sample()
+ * @since      File available since 11/11/2014
  */
 class Afdeling {
     /**
-     * Description of afdeling.
-     * @var type varchar
+     * Beschrijving van de afdeling.
+     * @var string $afdeling_desc
      */
     public $afdeling_desc;
     /**
-     * Id of afdeling.
-     * @var type int
+     * Het ID van de afdeling.
+     * @var int $afdeling_id
      */
     public $afdeling_id;
     /**
-     * Title of afdeling.
-     * @var type varchar
+     * Titel van de afdeling.
+     * @var string $afdeling_title
      */
     public $afdeling_title;
     /**
-     * Returns an array with errors.
-     * @var type array
-     * @return error array
+     * Bevat het Mysqli object.
+     * @var object Mysqli
      */
     public $mysqli;
-    private $db;
     /**
-     * Calls Item's constructor. 
+     * @var Item
+     */
+    private $db;
+
+    /**
+     * Calls Item's constructor.
      */
     public function __construct() {
         // insert name of the table, id field, title field and description field
         $this->db = new Item('afdeling', 'afdeling_id', 'afdeling_naam', 'afdeling_beschrijving');
     }
+
     /**
      * Adds afdeling using the inserted parameters and DbItem's functions.
      * @param type $afdeling_title
      * @param type $afdeling_desc
      * @return boolean
      */
-    public function add($afdeling_title, $afdeling_desc){
+    public function add($afdeling_title, $afdeling_desc) {
         // if title and desc are empty:
-        if(empty($afdeling_title) and empty($afdeling_desc)){
+        if(empty($afdeling_title) and empty($afdeling_desc)) {
             // return error
             echo ERROR_MISSING_BOTH;
             return FALSE;
-        // if title is empty
-        }elseif(empty($afdeling_title)){
+            // if title is empty
+        } elseif(empty($afdeling_title)) {
             // return error
             echo ERROR_MISSING_TITLE;
             return FALSE;
-        // if description is empty
-        }elseif(empty($afdeling_desc)){
-             // return error
+            // if description is empty
+        } elseif(empty($afdeling_desc)) {
+            // return error
             ERROR_MISSING_DESC;
             return FALSE;
-        // if both vars are filled
-        }elseif(!empty($afdeling_title) or !empty($afdeling_desc)){
+            // if both vars are filled
+        } elseif(!empty($afdeling_title) or !empty($afdeling_desc)) {
             // execute DbItem's addItem using inserted parameters
             return $this->db->addItem($afdeling_title, $afdeling_desc);
         }
-        
+
     }
+
     /**
      * Edits afdeling using the inserted parameters and DbItem's functions.
      * @param type $afdeling_id
@@ -69,33 +85,55 @@ class Afdeling {
      * @param type $afdeling_desc
      * @return type bool
      */
-    public function edit($afdeling_id, $afdeling_title, $afdeling_desc){
+    public function edit($afdeling_id, $afdeling_title, $afdeling_desc) {
         return $this->db->editItem($afdeling_id, $afdeling_title, $afdeling_desc);
     }
+
     /**
-     * Gets afdeling by id from database using DbItem's getItemById. 
+     * Gets afdeling by id from database using DbItem's getItemById.
      * @param type $afdeling_id
      * @return type array
      */
-    public function getAfdelingById($afdeling_id){
+    public function getAfdelingById($afdeling_id) {
         return $this->db->getItemById($afdeling_id);
-        
+
     }
+
     /**
      * Gets entire list from database using DbItem's getItemList.
      * @return type array
      */
-    public function getList(){
+    public function getList() {
         return $this->db->getList();
     }
-    public function setTitel($value) {
-        $this->afdeling_title = $value;
+
+    /**
+     * Zet de Titel van de Afdeling in object.
+     * @param string $title Titel van de Afdeling
+     * @return void
+     */
+    public function setTitel($title) {
+        if(!empty($title)) {
+            $this->afdeling_title = $title;
+        }
     }
-    public function setBeschrijving($value) {
-        $this->afdeling_desc = $value;
+
+    /**
+     * Zet de beschrijving in attribuut.
+     * @param string $desc Beschrijving van Afdeling
+     * @return void
+     */
+    public function setBeschrijving($desc) {
+        if(!empty($desc)) {
+            $this->afdeling_desc = $desc;
+        }
     }
+
     public function create() {
-        $this->db->createAfdeling($this->afdeling_title, $this->afdeling_desc);
+        if(!empty($this->afdeling_id) && !empty($this->afdeling_desc)) {
+            $this->db->createAfdeling($this->afdeling_title, $this->afdeling_desc);
+        }
     }
 }
+
 ?>
